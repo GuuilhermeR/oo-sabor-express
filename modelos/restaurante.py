200524912559687b90039a675e9b7b3a8645808f
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-13 20:28:59 -0300

    02. Construtor e instanciando objetos - Métodos especiais

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -3,6 +3,9 @@ class Restaurante:
         self.nome = nome
         self.categoria = categoria
         self.ativo = False
+    
+    def __str__(self):
+        return f'{self.nome} | {self.categoria}'
 
 restaurante_praca = Restaurante('Praça','Gourmet')
 restaurante_pizza = Restaurante('Pizza Express','Italiana')
