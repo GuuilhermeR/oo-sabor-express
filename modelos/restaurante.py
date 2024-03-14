167337fd8e5bd83f66ff1a60842e7ad1b67af363
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-14 18:12:36 -0300

    03.Property e métodos de classe - 03. Aprofundando em propriedades

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,24 +2,24 @@ class Restaurante:
     restaurantes = []
     
     def __init__(self, nome, categoria): #Construtor da classe
-        self.nome = nome
-        self.categoria = categoria
+        self._nome = nome.title()
+        self._categoria = categoria.upper()
         self._ativo = False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'{self.nome} | {self.categoria}'
+        return f'{self._nome} | {self._categoria}'
     
     def listar_restaurantes():
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '👌' if self._ativo else '🤦‍♂️'
 
-restaurante_praca = Restaurante('Praça','Gourmet')
-restaurante_pizza = Restaurante('Pizza Express','Italiana')
+restaurante_praca = Restaurante('praça','Gourmet')
+restaurante_pizza = Restaurante('pizza express','Italiana')
 
 Restaurante.listar_restaurantes()
