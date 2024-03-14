ca67f8836468dcc3a690e468ddc71194de9fbf3c
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-14 18:17:06 -0300

    03.Property e métodos de classe - 04 Métodos de classes

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -10,16 +10,21 @@ class Restaurante:
     def __str__(self):
         return f'{self._nome} | {self._categoria}'
     
-    def listar_restaurantes():
+    @classmethod
+    def listar_restaurantes(cls):
         print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'.ljust(25)}')
-        for restaurante in Restaurante.restaurantes:
+        for restaurante in cls.restaurantes:
             print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {restaurante.ativo}')
 
     @property
     def ativo(self):
         return '👌' if self._ativo else '🤦‍♂️'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
 restaurante_praca = Restaurante('praça','Gourmet')
+restaurante_praca.alternar_estado()
 restaurante_pizza = Restaurante('pizza express','Italiana')
 
 Restaurante.listar_restaurantes()
