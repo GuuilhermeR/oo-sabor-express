5ea192d40a8264c90c6eddea2114e027943bed77
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-18 20:12:04 -0300

    02. Polimorfismo e método abstrato - 02. Métodos para adicionar itens.

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ from modelos.cardapio.prato import Prato
 restaurante_praca = Restaurante('Praça', 'Gourmet')
 bebida_suco = Bebida('Suco de Melância', 5.00, 'Grande')
 prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
-
+restaurante_praca.adicionar_bebida_no_cardapio(bebida_suco)
+restaurante_praca.adicionar_prato_no_cardapio(prato_paozinho)
 
 def main():
     print(bebida_suco)
