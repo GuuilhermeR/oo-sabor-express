f3047b67796e144c0e861bdf11217dc35ca23e51
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-18 20:01:12 -0300

    Orientação a Objetos e consumo de API - 04. Herança

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,3 +1,6 @@
-class Prato:
+from modelos.cardapio.item_cardapio import ItemCardapio
+
+class Prato(ItemCardapio): #Isso diz que o Prato é filho do ItemCardapio e que ele irá herdar classe, atributos do ItemCardapio.
     def __init__(self, nome, preco, descricao):
-        pass
+        super().__init__(nome, preco) #O super ele permite que acessamos as informações de outras classes (métodos e atributos).
+        self._descricacao=descricao
