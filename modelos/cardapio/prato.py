71bf2c546f67f133f64330f83ad0352a004c47b3
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-18 20:34:13 -0300

    04. Exibindo o cardápio.

--- modelos/cardapio/prato.py
+++ modelos/cardapio/prato.py
@@ -1,9 +1,9 @@
 from modelos.cardapio.item_cardapio import ItemCardapio
 
 class Prato(ItemCardapio): #Isso diz que o Prato é filho do ItemCardapio e que ele irá herdar classe, atributos do ItemCardapio.
-    def __init__(self, nome, preco, descricao):
-        super().__init__(nome, preco) #O super ele permite que acessamos as informações de outras classes (métodos e atributos).
-        self._descricacao=descricao
-        
+    def __init__(self,nome,preco,descricao):
+        super().__init__(nome,preco) #O super ele permite que acessamos as informações de outras classes (métodos e atributos).
+        self.descricao = descricao 
+
     def __str__(self):
         return self._nome
