d74272082da4c2c3428af8863557ba5527023359
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-18 19:56:11 -0300

    Orientação a Objeto e consumo API - 01. Herança Criando a classe ItemCardapio

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -2,8 +2,8 @@ from modelos.avaliacao import Avaliacao
 
 class Restaurante:
     restaurantes = []
-    
-    def __init__(self, nome, categoria): #Construtor da classe
+
+    def __init__(self, nome, categoria):
         self._nome = nome.title()
         self._categoria = categoria.upper()
         self._ativo = False
@@ -15,27 +15,27 @@ class Restaurante:
     
     @classmethod
     def listar_restaurantes(cls):
-        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} | {'Status'}')
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Avaliação'.ljust(25)} |{'Status'}')
         for restaurante in cls.restaurantes:
-            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} | {restaurante.ativo}')
+            print(f'{restaurante._nome.ljust(25)} | {restaurante._categoria.ljust(25)} | {str(restaurante.media_avaliacoes).ljust(25)} |{restaurante.ativo}')
 
     @property
     def ativo(self):
-        return '👌' if self._ativo else '🤦‍♂️'
+        return '⌧' if self._ativo else '☐'
     
     def alternar_estado(self):
         self._ativo = not self._ativo
-        
+
     def receber_avaliacao(self, cliente, nota):
-        if 0 < int(nota) <= 5:
+        if 0 < nota <= 5: 
             avaliacao = Avaliacao(cliente, nota)
             self._avaliacao.append(avaliacao)
-        
-    @property #Permite que seja possível ler essas informações e não alteradas
+
+    @property
     def media_avaliacoes(self):
         if not self._avaliacao:
-            return "NOVO"
-        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao) #ternário: Pega todas avaliações e a unica que eu quero é a nota
+            return '-'
+        soma_das_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
         quantidade_de_notas = len(self._avaliacao)
-        media = round(soma_das_notas/quantidade_de_notas, 1)
+        media = round(soma_das_notas / quantidade_de_notas, 1)
         return media
